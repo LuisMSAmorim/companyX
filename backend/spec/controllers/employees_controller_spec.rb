13a8a9297174eb0e633f37876709a1db58224245
LuisMSAmorim <luismiguelsilvaamorim@gmail.com>
2022-08-23 19:45:07 -0300

    Add tests

--- backend/spec/controllers/employees_controller_spec.rb
+++ backend/spec/controllers/employees_controller_spec.rb
@@ -52,4 +52,42 @@ RSpec.describe EmployeesController, type: :controller do
       end
     end
   end
+
+  describe "#update" do
+    context "when successful" do
+      subject { patch :update, params: { id: @employee.id, employee: { first_name: "New Name" } } }
+      it "returns the updated employee" do
+        parsed_response = JSON.parse(subject.body)
+        expect(parsed_response["id"]).to eq(@employee.id)
+      end
+      it "returns http status ok" do
+        expect(subject).to have_http_status(:ok)
+      end
+      it "updates the employee" do
+        expect { subject }.to change { Employee.last.first_name }.to("New Name")
+      end
+    end
+    context "when failure" do
+      subject { patch :update, params: { id: @employee.id, employee: { first_name: nil } } }
+      it "returns http status unprocessable_entity" do
+        expect(subject).to have_http_status(:unprocessable_entity)
+      end
+      it "returns the errors" do
+        expect(subject.body).to eq({first_name: ["can't be blank"]}.to_json)
+      end
+      it "does not update the employee" do
+        expect { subject }.to_not change { @employee.first_name }
+      end
+    end
+  end
+
+  describe "#destroy" do
+    subject { delete :destroy, params: { id: @employee.id } }
+    it "returns http status no_content" do
+      expect(subject).to have_http_status(:no_content)
+    end
+    it "deletes the employee" do
+      expect { subject }.to change { Employee.count }.by(-1)
+    end
+  end
 end
