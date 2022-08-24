d71f7589431b397a62a2a2c2c27223c565088916
LuisMSAmorim <luismiguelsilvaamorim@gmail.com>
2022-08-24 10:21:14 -0300

    Add trait to departments factory

--- backend/spec/factories/departments.rb
+++ backend/spec/factories/departments.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :department do
     sequence(:name) { |n| "Department #{n}" }
+    trait :with_employees do
+      after(:create) do |department|
+        create_list(:employee, 3, department: department)
+      end
+    end
   end
 end
